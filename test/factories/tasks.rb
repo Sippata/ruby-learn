fbc99b6302bce72593d403b41ba0bfaad6844a43
Grigory Kaniev <netsafe.g@gmail.com>
2020-11-06 12:08:39 +0500

    Remove state field from the task factory

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -4,7 +4,6 @@ FactoryBot.define do
     description { 'MyText' }
     author factory: :manager
     assignee factory: :developer
-    state { 'MyString' }
     expired_at { '2020-10-31' }
   end
 end

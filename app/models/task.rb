0ef6cd69661d4d928c8954390efaf36dbdc89969
Sippata <41334863+Sippata@users.noreply.github.com>
2020-11-11 10:18:23 +0500

    Add state machine for Task model (#7)
    * Add state machine for Task model
    
    * Fix misprints
    
    * Fix declaration of state mchine events
    
    * Remove state field from the task factory

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,4 +6,13 @@ class Task < ApplicationRecord
   validates :description, presence: true
   validates :author, presence: true
   validates :description, length: { maximum: 500 }
+
+  state_machine initial: :new_task do
+    event(:archive) { transition [:new_task, :released] => :archived }
+    event(:send_for_development) { transition [:new_task, :in_qa, :in_code_review] => :in_development }
+    event(:send_for_qa) { transition in_development: :in_qa }
+    event(:send_for_code_review) { transition in_qa: :in_code_review }
+    event(:send_for_ready_for_release) { transition in_code_review: :ready_for_release }
+    event(:send_for_release) { transition ready_for_release: :released }
+  end
 end

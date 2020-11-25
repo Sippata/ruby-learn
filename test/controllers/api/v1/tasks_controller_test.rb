ffb7b7e07ffd654fecaaa17b4600b0e8cdb3c340
Grigory Kaniev <netsafe.g@gmail.com>
2020-11-25 18:55:04 +0500

    Send mail when task create, updatate or delete

--- test/controllers/api/v1/tasks_controller_test.rb
+++ test/controllers/api/v1/tasks_controller_test.rb
@@ -19,7 +19,9 @@ class Api::V1::TasksControllerTest < ActionController::TestCase
     assignee = create(:user)
     task_attributes = attributes_for(:task)
       .merge({ author_id: author.id, assignee_id: assignee.id })
-    post :create, params: { task: task_attributes, format: :json }
+    assert_emails 1 do
+      post :create, params: { task: task_attributes, format: :json }
+    end
     assert_response :created
 
     data = JSON.parse(response.body)
@@ -31,18 +33,28 @@ class Api::V1::TasksControllerTest < ActionController::TestCase
 
   test 'should put update' do
     author = create(:user)
+    sign_in(author)
     assignee = create(:user)
     task = create(:task, author: author)
     task_attributes = attributes_for(:task)
       .merge({ author_id: author.id, assignee_id: assignee.id })
       .stringify_keys
+    assert_emails 1 do
+      patch :update, params: { id: task.id, format: :json, task: task_attributes }
+    end
+    assert_response :success
+
+    task.reload
     assert_equal task.slice(*task_attributes.keys), task_attributes
   end
 
   test 'should delete destroy' do
     author = create(:user)
+    sign_in(author)
     task = create(:task, author: author)
-    delete :destroy, params: { id: task.id, format: :json }
+    assert_emails 1 do
+      delete :destroy, params: { id: task.id, format: :json }
+    end
     assert_response :success
 
     assert !Task.where(id: task.id).exists?
